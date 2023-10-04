ed5eb00ded214fd84c7513adb023bbf51065ca3a
JasonG12 <jasongdev@gmail.com>
2023-10-04 18:06:37 -0400

    add caching for static data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,20 @@
 
 
+
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import { FetchResponse } from './useData';
 import genres from '../data/genres';
 export interface Genre {
   id: number,
   name: string,    
   image_background: string
 }
-export const useGenres = () => ({data: genres, isLoading: false, error: null})
+export const useGenres = () => useQuery({
+  queryKey: ['genres'],
+  queryFn: () => apiClient
+    .get<FetchResponse<Genre>>('/genres')
+    .then(res => res.data) ,
+    staleTime : 60 * 60 * 24 * 1000, //24 hrs
+    initialData: {count: genres.length, results: genres}
+  })
