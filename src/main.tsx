837380499a9f700dd7aa1d06a4190631a64d0adb
jason g <jasongdev@gmail.com>
2023-11-17 13:10:01 -0500

    initial page route setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App.tsx";
-import "./index.css";
 import theme from "./theme.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         initialColorMode={theme.config.initialColorMode}
       ></ColorModeScript>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router}></RouterProvider>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
