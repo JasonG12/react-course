c2f5955b00a5b137bb5346a698726150914756ac
JasonG12 <jasongdev@gmail.com>
2024-02-09 15:21:14 -0500

    confige routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,19 @@
-import {createBrowserRouter} from 'react-router-dom'
-import Layout from './pages/Layout'
-import HomePage from './pages/HomePage'
-import GameDetailPage from './pages/GameDetailPage';
-import ErrorPage from './pages/ErrorPage';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import HomePage from "./pages/HomePage";
+import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout></Layout>,
-        errorElement: <ErrorPage />,
-        children: [
-            {index: true, element: <HomePage/>},
-            {path: 'games/:slug', element: <GameDetailPage/> }
-        ]
-    }
-])
+  {
+    path: "/react-course",
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "games/:slug", element: <GameDetailPage /> },
+    ],
+  },
+]);
 
 export default router;
