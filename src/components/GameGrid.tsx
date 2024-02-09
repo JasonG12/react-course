a5c451ddd8a2e7d111ebf6a84f72b92bec408b1f
JasonG12 <jasongdev@gmail.com>
2024-02-09 13:58:43 -0500

    add key to page fragment

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,11 +6,9 @@ import GameCardContainer from "./GameCardContainer";
 import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-
 const GameGrid = () => {
   const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
-
   if (error) return error && <Text>{error.message}</Text>;
   const fetchedGamesCount =
     data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
@@ -34,8 +32,8 @@ const GameGrid = () => {
                 <GameCardSkeleton></GameCardSkeleton>
               </GameCardContainer>
             ))}
-          {data?.pages.map((page) => (
-            <React.Fragment>
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
               {page.results.map((game) => (
                 <GameCardContainer key={game.id}>
                   <GameCard game={game}></GameCard>
