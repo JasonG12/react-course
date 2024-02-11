cc9733b9b37528d3c9fea72c47006957258ca72a
JasonG12 <jasongdev@gmail.com>
2024-02-10 20:22:39 -0500

    include genre data

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,3 +1,4 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
 
 
@@ -8,6 +9,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform; }[];
   slug: string;
+  genres:Genre[];
   metacritic: number;
   rating_top: number;
   rating: number;
